f0df19aac59fdc7607535b86ebf6377b0f08eb63
Ahammed shefin <ahammedshefin1@gmail.com>
2023-12-13 17:26:30 +0530

    inital commit

---
+++ index.js.js
@@ -0,0 +1,30 @@
+// importing modules
+const express=require("express")
+const path=require('path')
+// assigning express function to a variable named app
+const app=express()
+// app.get will take the request and will recieve a response from server
+//app.use is used to create a middle ware 
+//middle ware is used to eork a data before doing a proccess
+app.use(function(req,res,next){
+    console.log("middle-ware");
+    next()
+})
+
+app.get('/',function(req,res){   
+    // res.send works like write function in node
+    res.send("hai iam ahammed shefin")
+})
+app.get('/signup',function(req,res){
+    res.send("this is signup")
+
+})
+app.post('/data',function(req,res){
+    res.send('<h1> hai '+req.query.firstname+'</h1>')
+})
+
+
+// app.listen will create a port
+app.listen(3000,function(){
+    console.log("server started")
+})
